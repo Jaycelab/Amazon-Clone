f37d2f8df3660592de8cccdf2e145db07056ae0e
Jason Alvarado <jayxalvarado@gmail.com>
2025-01-01 19:37:14 -0800

    Remove unused imports from the main page component

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,3 @@
-import Image from 'next/image'
-import { Button } from '@/components/ui/button'
-import Menu from '@/components/shared/header/menu'
 import Header from '@/components/shared/header'
 import Search from '@/components/shared/header/search'
 
