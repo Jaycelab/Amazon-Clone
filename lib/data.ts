f5c7b465d2085afea498411c3bd1ed90f10d1aef
Jason Alvarado <jayxalvarado@gmail.com>
2025-01-03 14:44:12 -0800

    Add home carousel component with autoplay functionality and update carousel data

--- lib/data.ts
+++ lib/data.ts
@@ -39,6 +39,80 @@ const data = {
       href: '/page/help',
     },
   ],
+
+  //carousel data
+  carousels: [
+    {
+      title: 'Most Popular Shoes For Sale',
+      buttonCaption: 'Shop Now',
+      image: '/images/banner3.jpg',
+      url: '/search?category=Shoes',
+      isPublished: true, //if false, the carousel will not be displayed
+    },
+    {
+      title: 'Best Sellers in T-shirts',
+      buttonCaption: 'Shop Now',
+      image: '/images/banner1.jpg',
+      url: '/search?category=T-Shirts',
+      isPublished: true, //if false, the carousel will not be displayed
+    },
+    {
+      title: 'Best Deals on Wrist Watches',
+      buttonCaption: 'See More',
+      image: '/images/banner2.jpg',
+      url: '/search?category=Wrist-Watches',
+      isPublished: true, //if false, the carousel will not be displayed
+    },
+    {
+      title: 'Best Deals on 2',
+      buttonCaption: 'See More',
+      image: '/images/banner4.jpg',
+      url: '/search?category=Wrist-Watches',
+      isPublished: true, //if false, the carousel will not be displayed
+    },
+    {
+      title: 'Best Deals on 5',
+      buttonCaption: 'See More',
+      image: '/images/banner5.jpg',
+      url: '/search?category=Wrist-Watches',
+      isPublished: true, //if false, the carousel will not be displayed
+    },
+    {
+      title: 'Best Deals on 7',
+      buttonCaption: 'See More',
+      image: '/images/banner6.jpg',
+      url: '/search?category=Wrist-Watches',
+      isPublished: true, //if false, the carousel will not be displayed
+    },
+    {
+      title: 'Best Deals on Test',
+      buttonCaption: 'See More',
+      image: '/images/banner7.jpg',
+      url: '/search?category=Wrist-Watches',
+      isPublished: true, //if false, the carousel will not be displayed
+    },
+    {
+      title: 'Best Deals on 12',
+      buttonCaption: 'See More',
+      image: '/images/banner8.jpg',
+      url: '/search?category=Wrist-Watches',
+      isPublished: true, //if false, the carousel will not be displayed
+    },
+    {
+      title: 'Best Deals on W435',
+      buttonCaption: 'See More',
+      image: '/images/banner9.jpg',
+      url: '/search?category=Wrist-Watches',
+      isPublished: true, //if false, the carousel will not be displayed
+    },
+    {
+      title: 'Best Deals on 123',
+      buttonCaption: 'See More',
+      image: '/images/banner10.jpg',
+      url: '/search?category=Wrist-Watches',
+      isPublished: true, //if false, the carousel will not be displayed
+    },
+  ],
 }
 
 export default data
