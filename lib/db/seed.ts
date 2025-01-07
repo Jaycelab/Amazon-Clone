4f17e3dc00c0a2fc41ca4f921fc2eac99bb89ec1
Jason Alvarado <jayxalvarado@gmail.com>
2025-01-07 01:18:17 -0800

    1. Add database seeding script for product data and include environment variable loading
    2. Upload product images for product database

---
+++ lib/db/seed.ts
@@ -0,0 +1,26 @@
+import data from '@/lib/data'
+import { connectToDatabase } from '.'
+import Product from './models/product.model'
+import { cwd } from 'process'
+import { loadEnvConfig } from '@next/env'
+
+loadEnvConfig(cwd()) // load .env variables and call cwd() to get the current working directory
+
+const main = async () => {
+  try {
+    const { products } = data
+    await connectToDatabase(process.env.MONGODB_URI)
+
+    await Product.deleteMany()
+    const createdProducts = await Product.insertMany(products)
+
+    console.log({
+      createdProducts,
+      message: 'Seeeded database successfully',
+    })
+    process.exit(0)
+  } catch (error) {
+    console.error(error)
+    throw new Error('Failed to seed database')
+  }
+}
