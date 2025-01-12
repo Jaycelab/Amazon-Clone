416546777ced3ce416858897b3ab3853a7c10467
Jason Alvarado <jayxalvarado@gmail.com>
2025-01-11 16:39:33 -0800

    Add Best Selling Products slider to Home page

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -71,6 +71,7 @@ export default async function Page() {
   ]
 
   const todaysDeals = await getProductsByTag({ tag: 'todays-deal' })
+  const bestSellingProducts = await getProductsByTag({ tag: 'best-seller' })
 
   return (
     <>
@@ -81,6 +82,16 @@ export default async function Page() {
       <Card className='w-full rounded-none'>
         <CardContent className='p-4 items-center gap-3'>
           <ProductSlider title={"Today's Deals"} products={todaysDeals} />
+        </CardContent>
+      </Card>
+
+      <Card className='w-full rounded-none'>
+        <CardContent className='p-4 items-center gap-3'>
+          <ProductSlider
+            title='Best SElling Products'
+            products={bestSellingProducts}
+            hideDetails
+          ></ProductSlider>
         </CardContent>
       </Card>
     </>
