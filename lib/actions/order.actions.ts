3e396948bbab5ed9335e3810ba3ed82ca8ec83b3
Jason Alvarado <jayxalvarado@gmail.com>
2025-01-17 08:30:43 -0800

    Add order actions for delivery price calculation and update types for header menus

---
+++ lib/actions/order.actions.ts
@@ -0,0 +1,29 @@
+import { OrderItem } from '@/types'
+import { round2 } from '../utils'
+import { FREE_SHIPPING_MIN_PRICE } from '../constants'
+
+export const calcDeliveryDataAndPrice = async ({
+  items,
+}: {
+  deliveryDateIndex?: number
+  items: OrderItem[]
+}) => {
+  const itemsPrice = round2(
+    items.reduce((acc, item) => acc + item.price * item.quantity, 0) //reduce the items array to a single value
+  ) // 1. Calculate the total price of all items
+
+  const shippingPrice = itemsPrice > FREE_SHIPPING_MIN_PRICE ? 0 : 5 // 2. Calculate the shipping price
+  const taxPrice = round2(itemsPrice * 0.15) // 3. Calculate the tax price
+  const totalPrice = round2(
+    itemsPrice +
+      (shippingPrice ? round2(shippingPrice) : 0) +
+      (taxPrice ? round2(taxPrice) : 0)
+  ) // 4. Calculate the total price and round tax and shipping price to 2 decimal places
+
+  return {
+    itemsPrice,
+    shippingPrice,
+    taxPrice,
+    totalPrice,
+  }
+}
--- types/index.ts
+++ types/index.ts
@@ -7,6 +7,7 @@ export type IProductInput = z.infer<typeof ProductInputSchema>
 export type Data = {
   products: IProductInput[]
   headerMenus: {
+    
     name: string
     href: string
   }[]
